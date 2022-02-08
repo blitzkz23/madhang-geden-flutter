12ef42e056891a23d196514f303c6104064b5a36
blitzkz23 <blitzaldykazu@gmail.com>
2022-02-08 12:42:13 +0700

    resolve conflict with local

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,43 @@
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:http/http.dart';
+import 'package:provider/provider.dart';
+import 'package:restaurant_app/common/navigation.dart';
+import 'package:restaurant_app/data/db/database_helper.dart';
+import 'package:restaurant_app/data/preferences/preferences_helper.dart';
+import 'package:restaurant_app/page/add_review_page.dart';
 import 'package:restaurant_app/page/detail_page.dart';
 import 'package:restaurant_app/page/search_page.dart';
+import 'package:restaurant_app/provider/database_provider.dart';
+import 'package:restaurant_app/provider/preferences_provider.dart';
+import 'package:restaurant_app/provider/restaurant_post_review_provider.dart';
+import 'package:restaurant_app/provider/restaurant_provider.dart';
+import 'package:restaurant_app/provider/restaurant_search_provider.dart';
+import 'package:restaurant_app/provider/scheduling_provider.dart';
+import 'package:restaurant_app/utils/background_service.dart';
+import 'package:restaurant_app/utils/notification_helper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'common/styles.dart';
+import 'data/api/api_service.dart';
+import 'data/model/restaurant_detail.dart';
 import 'page/splash_screen.dart';
 import 'page/home_page.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final NotificationHelper _notificationHelper = NotificationHelper();
+  final BackgroundService _service = BackgroundService();
+
+  _service.initializeIsolate();
+
+  await AndroidAlarmManager.initialize();
+  await _notificationHelper.initNotifications(flutterLocalNotificationsPlugin);
+
   runApp(const MyApp());
 }
 
@@ -16,20 +47,53 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: FlexThemeData.light(
-            scheme: FlexScheme.sakura, textTheme: poppinsTheme),
-        initialRoute: SplashScreen.routeName,
-        routes: {
-          SplashScreen.routeName: (context) => const SplashScreen(),
-          HomePage.routeName: (context) => const HomePage(),
-          DetailPage.routeName: (context) => DetailPage(
-                restaurantId:
-                    ModalRoute.of(context)?.settings.arguments as String,
-              ),
-          SearchPage.routeName: (context) => const SearchPage(),
-        });
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<RestaurantProvider>(
+          create: (_) => RestaurantProvider(apiService: ApiService(Client())),
+        ),
+        ChangeNotifierProvider<RestaurantSearchProvider>(
+          create: (_) =>
+              RestaurantSearchProvider(apiService: ApiService(Client())),
+        ),
+        ChangeNotifierProvider<RestaurantPostReviewProvider>(
+          create: (_) =>
+              RestaurantPostReviewProvider(apiService: ApiService(Client())),
+        ),
+        ChangeNotifierProvider<DatabaseProvider>(
+          create: (_) => DatabaseProvider(databaseHelper: DatabaseHelper()),
+        ),
+        ChangeNotifierProvider<SchedulingProvider>(
+          create: (_) => SchedulingProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => PreferencesProvider(
+            preferencesHelper: PreferencesHelper(
+              SharedPreferences.getInstance(),
+            ),
+          ),
+        )
+      ],
+      child: MaterialApp(
+          navigatorKey: navigatorKey,
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: FlexThemeData.light(
+              scheme: FlexScheme.sakura, textTheme: poppinsTheme),
+          initialRoute: SplashScreen.routeName,
+          routes: {
+            SplashScreen.routeName: (context) => const SplashScreen(),
+            HomePage.routeName: (context) => const HomePage(),
+            DetailPage.routeName: (context) => DetailPage(
+                  restaurantId:
+                      ModalRoute.of(context)?.settings.arguments as String,
+                ),
+            SearchPage.routeName: (context) => const SearchPage(),
+            AddReviewPage.routeName: (context) => AddReviewPage(
+                  restaurant:
+                      ModalRoute.of(context)?.settings.arguments as Restaurant,
+                ),
+          }),
+    );
   }
 }
---
+++ lib/utils/background_service.dart
@@ -0,0 +1,36 @@
+import 'dart:ui';
+import 'dart:isolate';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:restaurant_app/data/api/api_service.dart';
+import 'package:restaurant_app/utils/notification_helper.dart';
+import 'package:restaurant_app/main.dart';
+
+final ReceivePort port = ReceivePort();
+
+class BackgroundService {
+  static BackgroundService? _instance;
+  static const String _isolateName = 'isolate';
+  static SendPort? _uiSendPort;
+
+  BackgroundService._internal() {
+    _instance = this;
+  }
+
+  factory BackgroundService() => _instance ?? BackgroundService._internal();
+
+  void initializeIsolate() {
+    IsolateNameServer.registerPortWithName(port.sendPort, _isolateName);
+  }
+
+  static Future<void> callback() async {
+    debugPrint('Alarm Fired!');
+    final NotificationHelper _notificationHelper = NotificationHelper();
+    var result = await ApiService(Client()).restaurantList();
+    await _notificationHelper.showNotification(
+        flutterLocalNotificationsPlugin, result);
+
+    _uiSendPort ??= IsolateNameServer.lookupPortByName(_isolateName);
+    _uiSendPort?.send(result);
+  }
+}
